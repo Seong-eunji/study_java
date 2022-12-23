c0b38dd9729a07c83cbc57dfdeb9f9e73e12e845
Seong-eunji <sejjes07@gmail.com>
2022-12-23 17:31:25 +0900

    Ambiguity

---
+++ ambiguity/src/ambiguityTest/ClassB.java
@@ -0,0 +1,9 @@
+package ambiguityTest;
+
+public class ClassB extends ClassA implements InterA {
+	@Override
+	public void printData() {
+		// TODO Auto-generated method stub
+		super.printData();
+	}
+}
---
+++ ambiguity/src/ambiguityTest/InterA.java
@@ -0,0 +1,8 @@
+package ambiguityTest;
+
+public interface InterA {
+	// abstract가 생략되어 있기 때문에 default를 입력해주어야 한다.
+	default void printData() {
+		System.out.println("InterA");
+	}
+}
